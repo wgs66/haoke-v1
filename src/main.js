1d5352ce3ef40ad55cd7c7b9653f76d630226826
wgs66 <707205590@qq.com>
2022-07-15 18:56:03 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible'
+import '@/assets/fonts/iconfont.css'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
